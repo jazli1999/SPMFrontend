d3e056931c04e36ae1065bdb957fa9e4911f398f
IanLi <jasmine_chieh_li@outlook.com>
2020-05-09 20:38:15 +0800

    通了！

--- src/components/QuakeInfo.vue
+++ src/components/QuakeInfo.vue
@@ -1,23 +1,25 @@
 <template>
     <div id="quakeinfo">
         <h1>地震灾情信息</h1>
-        <table v-if="showData">
+        <table v-show="showData">
             <thead>
-                <!-- <th>地点</th>
+                <th>地点</th>
                 <th>时间</th>
                 <th>编号</th>
                 <th>类型</th>
                 <th>破坏等级</th>
                 <th>上报单位</th>
-                <th>备注</th> -->
-
-                <th>name</th>
-                <th>desc</th>
+                <th>备注</th>
             </thead>
-            <tbody v-for="item in data" :key="item.name">
+            <tbody v-for="item in data" :key="item.id">
                 <tr>
-                    <td>{{ item.name }}</td>
-                    <td>{{ item.desc }}</td>
+                    <td>{{ item.location }}</td>
+                    <td>{{ item.date }}</td>
+                    <td>{{ item.id }}</td>
+                    <td>{{ item.type }}</td>
+                    <td>{{ item.grade }}</td>
+                    <td>{{ item.reportingunit }}</td>
+                    <td>{{ item.note }}</td>
                 </tr>
             </tbody>
         </table>
@@ -31,17 +33,36 @@ export default {
         return {
             url: '',
             showData: false,
-            data: [{
-                'name': 'a',
-                'desc': 'b'
-            }, {
-                'name': 'c',
-                'desc': 'd'
-            }]
+            data: []
         }
     },
     mounted: function () {
-        this.showData = true;
+        var _this = this;
+        const axios = require('axios');
+
+        axios.get("/api/disaster/CommonDisaster")
+        .then(function (response) {
+            _this.updateData(response.data);
+            _this.showData = true;
+            console.log(_this.data);
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+        .then(function () {
+            console.log('finished');
+        })
+    },
+    methods: {
+        updateData: function (rawData) {
+            const strData = rawData.split('}');
+            strData.pop();
+            this.data = [];
+            for (let i in strData) {
+                console.log(strData[i] + '}');
+                this.data[i] = JSON.parse(strData[i] + '}');   
+            }
+        }
     }
 
 }
--- vue.config.js
+++ vue.config.js
@@ -3,18 +3,18 @@ module.exports = {
     assetsDir: 'assets', //静态资源目录（js, css, img）
     lintOnSave: false, //是否开启eslint
     devServer: {
-        open: true, //是否自动弹出浏览器页面
-        host: "localhost", 
+        open: false, //是否自动弹出浏览器页面
+        host: "0.0.0.0", 
         port: '8081', 
         https: false,   //是否使用https协议
         hotOnly: false, //是否开启热更新
         proxy: {
-             '/test': {
-                target: 'http://47.103.5.100', //API服务器的地址
+             '/api': {
+                target: 'http://114.116.118.76:8080/', //API服务器的地址
                 ws: true,  //代理websockets
                 changeOrigin: true, // 虚拟的站点需要更管origin
-                pathRewrite: {   //重写路径 比如'/api/aaa/ccc'重写为'/aaa/ccc'
-                    '^/test': ''
+                pathRewrite: {
+                    '^/api': ''
                 }
             }
         }
