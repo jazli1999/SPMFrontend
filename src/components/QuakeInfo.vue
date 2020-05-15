6fae15949a261b4035588df4fa23d55e589e5189
IanLi <jasmine_chieh_li@outlook.com>
2020-05-16 00:11:25 +0800

    清理QuakeInfo

--- src/components/QuakeInfo.vue
+++ src/components/QuakeInfo.vue
@@ -21,35 +21,12 @@
                             </el-form>
                         </template>
                     </el-table-column>
-                    <el-table-column align="center" prop="id" label="编号" width="100px" >
-                        <template slot-scope="row_scope">
-                            <span v-if="row_scope.row.editting">
-                                <el-input size="mini" placeholder="请输入内容" v-model="row_scope.row.id">
-                                </el-input>
-                            </span>
-                            <span v-else>{{ row_scope.row.id }}</span>
-                        </template>
-                    </el-table-column>
+                    <el-table-column align="center" prop="id" label="编号" width="100px" />
                     <el-table-column align="center" prop="location" label="地点" />
                     <el-table-column align="center" sortable prop="grade" label="震级" width="80px"/>
                     <el-table-column align="center" sortable prop="date" label="日期时间" />
                     <el-table-column align="center" sortable prop="type" label="类型"/>
                     <el-table-column align="center" prop="reportingunit" label="上报单位"/>
-                    <el-table-column align="center" v-if="admin">
-                        <template slot="header" slot-scope="scope">
-                            <el-button size="mini" type="primary" @click="handleAdd(scope.$index, scope.row)">添加一行</el-button>
-                        </template>
-                        <template slot-scope="scope">
-                            <el-button v-if="scope.row.editing" size="mini" @click="handleSave(scope.$index, scope.row)">保存</el-button>
-                            <el-button v-else size="mini" @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
-                            <el-button v-if="scope.row.editing" size="mini" type="danger" @click="handleCancel(scope.$index, scope.row)">取消</el-button>
-                            <el-button v-else size="mini" type="danger" @click="handleDelete(scope.$index, scope.row)">删除</el-button>
-                            <!-- <span v-if="scope.row.editing" size="mini" @click="handleSave(scope.$index, scope.row)">保存</span>
-                            <span v-else size="mini" @click="handleEdit(scope.$index, scope.row)">编辑</span>
-                            <span v-if="scope.row.editing" size="mini" type="danger" @click="handleCancel(scope.$index, scope.row)">取消</span>
-                            <span v-else size="mini" type="danger" @click="handleDelete(scope.$index, scope.row)">删除</span> -->
-                        </template>
-                    </el-table-column>
                 </el-table>
             </div>
         </el-card>
@@ -97,9 +74,7 @@ export default {
             });
         },
         handleEdit: function(index, content) {
-            console.log(this.data[index].editing);
-            this.data[index].editing = true;
-            console.log(this.data[index].editing);
+
         }
     }
 
