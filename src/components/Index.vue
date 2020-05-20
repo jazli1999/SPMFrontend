d69ffed8559da5f402abd61b9742ad51cca81e0d
L2017211919 <liyanjie@bupt.edu.cn>
2020-05-21 02:41:05 +0800

    更新首页，添加操作指南入口

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,6 +1,7 @@
 <template>
     <div id='index'>
         <h1>Index</h1>
+        <h3>访问<a href="https://devcloud.huaweicloud.com/docman/project/e049f18d45a44befab1e6ab5168ce07e/docman/detail/3617201/list">平台操作手册</a></h3>
     </div>
 </template>
 
